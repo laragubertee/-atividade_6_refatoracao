05bb96edc6c14bd35b9dfa19b6ef2c328277a55a
laragubertee <lara_guberte@estudante.sesisenai.org.br>
2026-04-10 09:32:16 -0300

    Limpeza e Refatoração de Código

--- sistema-original.js
+++ sistema-original.js
@@ -1,158 +1,174 @@
 // sistema.js
 // Gerenciador de playlist de musicas
 
-var a = []
-var b = 0
-var c = false
-var d = ""
+var array = [];
+var tempoTotal = 0;
+var flag = false;
+var texto = "";
 
-function f1(x, y) {
-var r = x * 60 + y
-return r
+function tempoEmSegundos(minutos, segundos) {
+  var totalSegundos = minutos * 60 + segundos;
+  return totalSegundos;
 }
 
-function f2(s) {
-var m = Math.floor(s / 60)
-var seg = s % 60
-if(seg < 10){
-return m + ":0" + seg
-}
-return m + ":" + seg
+function duracao(param_segundos) {
+  var minutos = Math.floor(param_segundos / 60);
+  var segundos = param_segundos % 60;
+  if (segundos < 10) {
+    return minutos + ":0" + segundos;
+  }
+  return minutos + ":" + segundos;
 }
 
 function f3(lista, n) {
-var resultado = null
-for(var i = 0; i < lista.length; i++){
-if(lista[i].nome == n){
-resultado = lista[i]
-}
-}
-return resultado
-}
-
-function f4(v) {
-if(v != null){
-if(v >= 0){
-if(v <= 100){
-if(typeof v === "number"){
-return true
-}
-}
-}
-}
-return false
+  var resultado = null;
+  for (var i = 0; i < lista.length; i++) {
+    if (lista[i].nome == n) {
+      resultado = lista[i];
+    }
+  }
+  return resultado;
 }
 
-function f5(lista) {
-var t = 0
-for(var i = 0; i < lista.length; i++){
-t = t + lista[i].duracao
-}
-b = t
-return t
-}
-
-function f6(i) {
-if(i >= 0 && i < a.length){
-if(a[i].fav == false){
-a[i].fav = true
-} else {
-a[i].fav = false
-}
-}
+function testarVolume(volume) {
+  if (volume != null) {
+    if (volume >= 0) {
+      if (v <= 100) {
+        if (typeof v === "number") {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
 }
 
-function f7(lista, p) {
-var r = []
-for(var i = 0; i < lista.length; i++){
-if(lista[i].artista == p){
-r.push(lista[i])
-}
-}
-return r
+function duracaoSegundos(lista) {
+  var tempo = 0;
+  for (var i = 0; i < lista.length; i++) {
+    tempo = tempo + lista[i].duracao;
+  }
+  tempoTotal = tempo;
+  return tempo;
 }
 
-function f8(lista, g) {
-var r = []
-for(var i = 0; i < lista.length; i++){
-if(lista[i].genero == g){
-r.push(lista[i])
+function favoritos(musica) {
+  if (i >= 0 && i < musica.length) {
+    if (musica[i].favorito == false) {
+      musica[i].favorito = true;
+    } else {
+      musica[i].favorito = false;
+    }
+  }
 }
+
+function filtarArtista(lista, artista) {
+  var listaArtistaFiltrada = [];
+  for (var i = 0; i < lista.length; i++) {
+    if (lista[i].artista == artista) {
+      listaArtistaFiltrada.push(lista[i]);
+    }
+  }
+  return r;
 }
-return r
+
+function filtrarGenero(lista, genero) {
+  var listaFiltrada = [];
+  for (var i = 0; i < lista.length; i++) {
+    if (lista[i].genero == genero) {
+      listaFiltrada.push(lista[i]);
+    }
+  }
+  return r;
 }
 
 function f9(lista) {
-var ct = 0
-for(var i = 0; i < lista.length; i++){
-if(lista[i].fav == true){
-ct = ct + 1
-}
-}
-return ct
+  var ct = 0;
+  for (var i = 0; i < lista.length; i++) {
+    if (lista[i].fav == true) {
+      ct = ct + 1;
+    }
+  }
+  return ct;
 }
 
-function f10(lista) {
-var cp = lista.slice()
-cp.sort(function(x, y){
-if(x.nome < y.nome) return -1
-if(x.nome > y.nome) return 1
-return 0
-})
-return cp
+function ordenaLista(lista) {
+  var listaOrdenada = lista.slice();
+  listaOrdenada.sort(function (posicaoA, posicaoB) {
+    if (posicaoA.nome < posicaoB.nome) return -1;
+    if (posicaoA.nome > posicaoB.nome) return 1;
+    return 0;
+  });
+  return listaOrdenada;
 }
 
 function f11(lista, p1, p2) {
-if(p1 < 0 || p1 >= lista.length) return
-if(p2 < 0 || p2 >= lista.length) return
-var tmp = lista[p1]
-lista[p1] = lista[p2]
-lista[p2] = tmp
+  if (p1 < 0 || p1 >= lista.length) return;
+  if (p2 < 0 || p2 >= lista.length) return;
+  var tmp = lista[p1];
+  lista[p1] = lista[p2];
+  lista[p2] = tmp;
 }
 
 function f12(lista, lim) {
-var r = []
-for(var i = 0; i < lista.length; i++){
-if(lista[i].duracao <= lim){
-r.push(lista[i])
-}
-}
-return r
+  var r = [];
+  for (var i = 0; i < lista.length; i++) {
+    if (lista[i].duracao <= lim) {
+      r.push(lista[i]);
+    }
+  }
+  return r;
 }
 
-function add(nm, art, gen, min, seg) {
-var obj = {}
-obj.nome = nm
-obj.artista = art
-obj.genero = gen
-obj.duracao = f1(min, seg)
-obj.fav = false
-a.push(obj)
+function adicionaListaMusica(nome, artista, genero, minutos, segundos) {
+  var obj = {};
+  obj.nome = nome;
+  obj.artista = artista;
+  obj.genero = genero;
+  obj.duracao = f1(minutos, segundos);
+  obj.favorita = false;
+  a.push(obj);
 }
 
 function mostra() {
-document.getElementById('musica0').innerHTML = a[0].nome + " - " + a[0].artista + " (" + f2(a[0].duracao) + ")"
-document.getElementById('musica1').innerHTML = a[1].nome + " - " + a[1].artista + " (" + f2(a[1].duracao) + ")"
-document.getElementById('musica2').innerHTML = a[2].nome + " - " + a[2].artista + " (" + f2(a[2].duracao) + ")"
-document.getElementById('musica3').innerHTML = a[3].nome + " - " + a[3].artista + " (" + f2(a[3].duracao) + ")"
-document.getElementById('musica4').innerHTML = a[4].nome + " - " + a[4].artista + " (" + f2(a[4].duracao) + ")"
+  document.getElementById("musica0").innerHTML =
+    a[0].nome + " - " + a[0].artista + " (" + duracao(a[0].duracao) + ")";
+  document.getElementById("musica1").innerHTML =
+    a[1].nome + " - " + a[1].artista + " (" + duracao(a[1].duracao) + ")";
+  document.getElementById("musica2").innerHTML =
+    a[2].nome + " - " + a[2].artista + " (" + duracao(a[2].duracao) + ")";
+  document.getElementById("musica3").innerHTML =
+    a[3].nome + " - " + a[3].artista + " (" + duracao(a[3].duracao) + ")";
+  document.getElementById("musica4").innerHTML =
+    a[4].nome + " - " + a[4].artista + " (" + duracao(a[4].duracao) + ")";
 }
 
-function rel() {
-var s = ""
-s = s + "=== RELATORIO DA PLAYLIST ===\n"
-s = s + "Total de musicas: " + a.length + "\n"
-s = s + "Favoritas: " + f9(a) + "\n"
-s = s + "Duracao total: " + f2(f5(a)) + "\n"
-s = s + "\n"
-for(var i = 0; i < a.length; i++){
-var fav = ""
-if(a[i].fav == true){
-fav = " [FAVORITA]"
-}
-s = s + (i + 1) + ". " + a[i].nome + " - " + a[i].artista + " (" + f2(a[i].duracao) + ")" + fav + "\n"
-}
-d = s
-console.log(s)
-return s
+function gerarEExibirRelatorio() {
+  var s = "";
+  s = s + "=== RELATORIO DA PLAYLIST ===\n";
+  s = s + "Total de musicas: " + a.length + "\n";
+  s = s + "Favoritas: " + f9(a) + "\n";
+  s = s + "Duracao total: " + duracao(duracaoSegundos(a)) + "\n";
+  s = s + "\n";
+  for (var i = 0; i < a.length; i++) {
+    var fav = "";
+    if (a[i].favoritas == true) {
+      favoritas = " [FAVORITA]";
+    }
+    s =
+      s +
+      (i + 1) +
+      ". " +
+      a[i].nome +
+      " - " +
+      a[i].artista +
+      " (" +
+      duracao(a[i].duracao) +
+      ")" +
+      favoritas +
+      "\n";
+  }
+  d = s;
+  console.log(s);
+  return s;
 }
